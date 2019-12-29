d6c07242d4ca6c81b9d22b6e1e3c0fa71b542625
jweir136 <jweir136@yahoo.com>
2019-12-28 22:21:15 -0500

    a

--- MNISTConvVAE.py
+++ MNISTConvVAE.py
@@ -10,7 +10,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(os.listdir("/storage"))
+print(os.listdir("/storage/mnist-jpg"))
 
 ################### SPECIFY THE DIRECTORIES AND TRANSFORMATIONS ###############################
 
